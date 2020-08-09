2d075d4fea14a0c49e51785a2b3526731d5061d9
Rúben <rav.rebelo@campus.fct.unl.pt>
2020-08-09 04:02:45 +0100

    Base Client from create-react-app

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import BaseImage from "./components/generator/base";
+
+
+function App() {
+  return (
+    <BaseImage/>
+  );
+}
+
+export default App;
---
+++ client/src/components/generator/base.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default function BaseImage() {
+
+    React.useEffect(() => {
+
+    })
+
+    return (<div>
+        <img src={'https://render-eu.worldofwarcraft.com/character/outland/215/168611799-main.jpg'} alt={'image'} style={{width: '75'}}/>
+    </div>);
+}
