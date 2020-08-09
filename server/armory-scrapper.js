28ca0d34e29f2a616510702e8dbb00a21b92fe64
Rúben <rav.rebelo@campus.fct.unl.pt>
2020-08-09 04:02:58 +0100

    WoW Armory Scrapper for Images

---
+++ server/armory-scrapper.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+module.exports = scrapper = function scrapArmory(callback) {
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://worldofwarcraft.com/en-gb/character/eu/outland/notrashy');
+    const avatarUrl = await page.evaluate((el) => window.getComputedStyle(el).backgroundImage, await page.$('.CharacterProfile-image > .Art-image'));
+    const backgroundImageUrl = await page.evaluate((el) => window.getComputedStyle(el).backgroundImage, await page.$('.Art-image'));
+    const backgroundImageCleaned = backgroundImageUrl.match(/url\("(.*)"/)[1] 
+    const avatarUrlCleaned = avatarUrl.match(/url\("(.*)"/)[1] 
+    await browser.close();
+    callback([backgroundImageCleaned, avatarUrlCleaned])
+  })();
+}
+
+
+
