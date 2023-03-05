a3f8d97ea4ad73445c7abfe55fcf7a6ff917da2d
Rúben Rebelo <rubenavrebelo@gmail.com>
2023-03-05 02:44:05 +0000

    LFS init fix

--- server/composite-image.js
+++ server/composite-image.js
@@ -1,81 +1,151 @@
-const Jimp = require('jimp');
+const Jimp = require("jimp");
 
 module.exports = {
-    compositeImages: function compositeImage(bg, imgAvatar, callback, hash) {
+  compositeImages: function compositeImage(bg, imgAvatar, callback, hash) {
     Jimp.read(bg, (err, img) => {
-        if(err) throw err
-        img.scale(1.15)
-        .crop(2200, 485, 305, 1200);
-        Jimp.read(imgAvatar, (err, avatar) => {
-            avatar.scale(1.4)
-            .crop(900, 450, 1295, 1200);
-            const image = new Jimp(1600, 1200, function (err, image) {
-                    image.composite(avatar,0, 0)
-                    .composite(img, 1295, 0);
-                    callback(image, hash);
-                });
-        })
-
-    })
-    },
-    writeStatus: async function writeStatus(image, unlock, text, callback) {
-        const mask = await Jimp.read('./static/images/status/border-mask.png');
-        const border = await Jimp.read('./static/images/status/border-transparent.png');
-        const status = await (await Jimp.read('./static/images/icons/Abilities/01.png')).scale(2);
-        status.mask(mask, 0, 0);
-        status.composite(border, 0, 0);
-        Jimp.loadFont('./static/fonts/font-60-yellow.fnt').then(yellowFont => {
-            Jimp.loadFont('./static/fonts/font-36.fnt').then(font => {
-                callback(null, image.composite(status,650, 550)
-                    .print(font, 780, 560, unlock)
-                    .print(yellowFont, 780, 610, text));
-            });
+      if (err) throw err;
+      img.scale(1.15).crop(2300, 452, 305, 1200);
+      Jimp.read(imgAvatar, (err, avatar) => {
+        avatar.scale(1.4).crop(1000, 450, 1295, 1200);
+        new Jimp(1600, 1200, function (err, image) {
+          image.composite(avatar, 0, 0).composite(img, 1295, 0);
+          callback(image, hash);
         });
-    },
+      });
+    });
+  },
+  writeStatus: async function writeStatus(
+    image,
+    unlock,
+    text,
+    shouldWrite,
+    callback
+  ) {
+    if (shouldWrite) {
+      const mask = await Jimp.read("./static/images/status/border-mask.png");
 
-    writeSkill: async function writeSkills(image, unlock, skill, callback) {
-        const mask = await Jimp.read('./static/images/status/border-mask.png');
-        const border = await Jimp.read('./static/images/status/border-transparent.png');
-        const status = await (await Jimp.read('./static/images/icons/Abilities/02_2.png')).scale(2);
-        status.mask(mask, 0, 0);
-        status.composite(border, 0, 0);
-        Jimp.loadFont('./static/fonts/font-60-yellow.fnt').then(yellowFont => {
-            Jimp.loadFont('./static/fonts/font-36.fnt').then(font => {
-                callback(null, image.composite(status,650, 750)
-                    .print(font, 780, 760, unlock)
-                    .print(yellowFont, 780, 810, skill));
-            });
+      const status = await (
+        await Jimp.read("./static/images/icons/ICONS/classes/Ability_Parry.PNG")
+      ).scale(1.9);
+      status.mask(mask, 0, 0);
+      Jimp.loadFont("./static/fonts/font-60-yellow.fnt").then((yellowFont) => {
+        Jimp.loadFont("./static/fonts/font-36.fnt").then((font) => {
+          callback(
+            null,
+            image
+              .composite(status, 650, 550)
+              .print(font, 780, 560, unlock)
+              .print(yellowFont, 780, 610, text)
+          );
         });
-    },
-    writeAchievement: function achievementEdit(image, text, hasFeats, callback) {
-        Jimp.loadFont('./static/fonts/font-24-white.fnt').then(font => {
-            Jimp.read('./static/images/achievement/template.png', (err, img) => {
-                Jimp.read('./static/images/achievement/cloth.jpg', (err, avatar) => {
-                    Jimp.read('./static/images/achievement/feats-of-strength-2x.png', (err, feats) => {
-                        let achievement = img.composite(avatar.scale(1.65), 35, 45)
-                        .print(font, 0, 3, {text, alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE}, 640, 180);
-                        if(hasFeats) {
-                            achievement = achievement.composite(feats, 505, 50)
-                        }
-                        callback(null, image.composite(achievement, 775, 900))
-                    });
-                });
-            });
-    
-        })
-    },
-    levelUp: async function levelUp(image, firstText, levelUp, callback) {
-        const levelbase = await Jimp.read('./static/images/level-up/base.png');
-        Jimp.loadFont('./static/fonts/font-84-white.fnt').then(white84Font => {
-            Jimp.loadFont('./static/fonts/font-106-yellow.fnt').then(yellowFont => {
-                levelbase.print(yellowFont, 0, 0, {text: levelUp, alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM}, 942, 249)
-                .print(white84Font, 0, 25, {text: firstText, alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_TOP}, 942, 249);
-                callback(null, image.composite(levelbase,650, 250));
-            });
-                    
+      });
+    } else {
+      callback(null, image);
+    }
+  },
+
+  writeSkill: async function writeSkills(
+    image,
+    unlock,
+    skill,
+    shouldWrite,
+    callback
+  ) {
+    if (shouldWrite) {
+      const mask = await Jimp.read("./static/images/status/border-mask.png");
+      const status = await (
+        await Jimp.read(
+          "./static/images/icons/ICONS/classes/evoker/Ability_Evoker_ChargedBlast.PNG"
+        )
+      ).scale(1.9);
+      status.mask(mask, 0, 0);
+      Jimp.loadFont("./static/fonts/font-60-yellow.fnt").then((yellowFont) => {
+        Jimp.loadFont("./static/fonts/font-36.fnt").then((font) => {
+          callback(
+            null,
+            image
+              .composite(status, 650, 750)
+              .print(font, 780, 760, unlock)
+              .print(yellowFont, 780, 810, skill)
+          );
         });
-    },
-}
-
-
-
+      });
+    } else {
+      callback(null, image);
+    }
+  },
+  writeAchievement: function achievementEdit(
+    image,
+    text,
+    hasFeats,
+    shouldWrite,
+    callback
+  ) {
+    if (shouldWrite) {
+      Jimp.loadFont("./static/fonts/font-24-white.fnt").then((font) => {
+        Jimp.read("./static/images/achievement/template.png", (err, img) => {
+          Jimp.read("./static/images/achievement/cloth.png", (err, avatar) => {
+            Jimp.read(
+              "./static/images/achievement/feats-of-strength-2x.png",
+              (err, feats) => {
+                let achievement = img
+                  .composite(avatar.scale(1.63), 29, 38)
+                  .print(
+                    font,
+                    0,
+                    3,
+                    {
+                      text,
+                      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+                      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+                    },
+                    640,
+                    180
+                  );
+                if (hasFeats) {
+                  achievement = achievement.composite(feats, 505, 50);
+                }
+                callback(null, image.composite(achievement, 775, 900));
+              }
+            );
+          });
+        });
+      });
+    } else {
+      callback(null, image);
+    }
+  },
+  levelUp: async function levelUp(image, firstText, levelUp, callback) {
+    const levelbase = await Jimp.read("./static/images/level-up/base.png");
+    Jimp.loadFont("./static/fonts/font-84-white.fnt").then((white84Font) => {
+      Jimp.loadFont("./static/fonts/font-106-yellow.fnt").then((yellowFont) => {
+        levelbase
+          .print(
+            yellowFont,
+            0,
+            0,
+            {
+              text: levelUp,
+              alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+              alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM,
+            },
+            942,
+            249
+          )
+          .print(
+            white84Font,
+            0,
+            25,
+            {
+              text: firstText,
+              alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+              alignmentY: Jimp.VERTICAL_ALIGN_TOP,
+            },
+            942,
+            249
+          );
+        callback(null, image.composite(levelbase, 650, 250));
+      });
+    });
+  },
+};
--- server/main.js
+++ server/main.js
@@ -1,60 +1,115 @@
 const express = require("express");
-const app = express()
+const app = express();
 const scrapper = require("./armory-scrapper");
-const {writeAchievement, writeStatus, writeSkill, levelUp} = require("./composite-image");
-var crypto = require('crypto');
-var fs = require('fs');
-const findRemoveSync = require('find-remove');
-const cron = require('node-cron');
-const Jimp = require('jimp');
+const {
+  writeAchievement,
+  writeStatus,
+  writeSkill,
+  levelUp,
+} = require("./composite-image");
+var crypto = require("crypto");
+var fs = require("fs");
+const findRemoveSync = require("find-remove");
+const cron = require("node-cron");
+const Jimp = require("jimp");
+app.use(express.json());
 
-const port = 8080
+const port = 8080;
 
-// Task Schedule for deleting folders every 24h
-cron.schedule('0 0 * * *', () => {
-    console.log('Deleting old folders....');
-    var result = findRemoveSync('/tmp', {age: {seconds: 86400}, dir: '*'})
+// Task Schedule for deletinng folders every 24h
+cron.schedule("0 0 * * *", () => {
+  console.log("Deleting old folders....");
+  var result = findRemoveSync("/tmp", { age: { seconds: 86400 }, dir: "*" });
 });
 
 function setBaseImage(image, hash) {
-    image.writeAsync(`tmp/${hash}/${hash}-base.jpg`)
-    console.log(`Wrote image in: tmp/${hash}/${hash}-base.jpg`)
+  image.writeAsync(`tmp/${hash}/${hash}-base.jpg`);
+  console.log(`Wrote image in: tmp/${hash}/${hash}-base.jpg`);
 }
 
 app.listen(port, () => {
-    console.log("Server started");
+  console.log("Server started");
 });
 
-app.get('/:realm/:character/base', async (req, res) => {
-    var hash = crypto.createHash('sha256').update(req.params.character.toLowerCase()+"-"+req.params.realm.toLowerCase()).digest('base64');
-    if(hash.includes('/')) hash = hash.replace('/', '$')
-    if (!fs.existsSync("tmp/"+hash)){
-        fs.mkdirSync("tmp/"+hash);
-    }
-    scrapper(req.params.character, req.params.realm, setBaseImage, hash);
-    res.sendStatus(200);
-})
+app.get("/:realm/:character/base", async (req, res) => {
+  var hash = crypto
+    .createHash("sha256")
+    .update(
+      req.params.character.toLowerCase() + "-" + req.params.realm.toLowerCase()
+    )
+    .digest("base64");
+  if (hash.includes("/")) hash = hash.replace("/", "$");
+  if (!fs.existsSync("tmp")) fs.mkdirSync("tmp");
+  if (!fs.existsSync("tmp/" + hash)) {
+    fs.mkdirSync("tmp/" + hash);
+  }
+  scrapper(req.params.character, req.params.realm, setBaseImage, hash);
+  res.sendStatus(200);
+});
 
-app.get('/:realm/:character/final', async (req, res) => {
-    var hash = crypto.createHash('sha256').update(req.params.character.toLowerCase()+"-"+req.params.realm.toLowerCase()).digest('base64');
-    if(hash.includes('/')) hash = hash.replace('/', '$')
-    if(fs.existsSync("tmp/"+hash) && fs.existsSync(`tmp/${hash}/${hash}-base.jpg`)){
-        let baseImage = await Jimp.read(`tmp/${hash}/${hash}-base.jpg`);
-        writeSkill(baseImage, "New spell learned", "Toxic Spray", function(err, image){
-            writeStatus(image, "Your responsabilities increased!", "Aturar putas", function(err, imageSkills){
-                writeAchievement(imageSkills, "Administrator", true, function(err, imageAchiev) {
-                    levelUp(imageAchiev, "You've reached", "Level Admin", function(err, final){
-                        final.writeAsync(`tmp/${hash}/${hash}-final.jpg`);
-                        res.sendFile(__dirname + `/tmp/${hash}/${hash}-final.jpg`);
-                    });
-                })
-            })
-        });
-        res.sendStatus(200);
-    } else {
-        res.sendStatus(404);
-    }
-})
+app.get("/:realm/:character/final", async (req, res) => {
+  console.log(req.body);
+  var hash = crypto
+    .createHash("sha256")
+    .update(
+      req.params.character.toLowerCase() + "-" + req.params.realm.toLowerCase()
+    )
+    .digest("base64");
+  if (hash.includes("/")) hash = hash.replace("/", "$");
+  if (
+    fs.existsSync("tmp/" + hash) &&
+    fs.existsSync(`tmp/${hash}/${hash}-base.jpg`)
+  ) {
+    let baseImage = await Jimp.read(`tmp/${hash}/${hash}-base.jpg`);
+    const {
+      spell2Title,
+      spell2Text,
+      spell2Icon,
+      spellText,
+      spellIcon,
+      spellTitle,
+      mainTitle,
+      mainLevel,
+      achievementText,
+    } = req.body.values;
+    const { spell, spell2, achievement } = req.body.config;
+    writeSkill(
+      baseImage,
+      spell2Title,
+      spell2Text,
+      spell2,
+      function (err, image) {
+        writeStatus(
+          image,
+          spellTitle,
+          spellText,
+          spell,
+          function (err, imageSkills) {
+            writeAchievement(
+              imageSkills,
+              achievementText,
+              true,
+              achievement,
+              function (err, imageAchiev) {
+                levelUp(
+                  imageAchiev,
+                  mainTitle,
+                  mainLevel,
+                  function (err, final) {
+                    final.writeAsync(`tmp/${hash}/${hash}-final.jpg`);
+                    res.sendFile(__dirname + `/tmp/${hash}/${hash}-final.jpg`);
+                  }
+                );
+              }
+            );
+          }
+        );
+      }
+    );
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 /* LEGACY CODE
 USED FOR INDIVIDUAL TESTING
@@ -124,4 +179,3 @@ function setCurrentImage(image) {
     image.writeAsync(`test/final.png`);
 }
 */
-
